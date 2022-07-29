9f6f9b978c0c9bec41826ebd9682aca42544fb72
Adam West <Adamwest023@gmail.com>
2022-07-29 17:42:41 -0500

    added animations array and menu to select dropdown

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+let playerState =  "idle";
+const dropDown = document.getElementById('animations');
+dropDown.addEventListener('change', function (e) {
+    playerState = e.target.value;
+});
+
 const canvas = document.getElementById('canvas1');
 const ctx = canvas.getContext('2d');
 
@@ -9,27 +15,83 @@ const playerImage = new Image();
 playerImage.src = 'assets/images/shadow_dog.png';
 const spriteWidth = 575;
 const spriteHeight = 523;
-let frameX = 0;
-let frameY = 0;
 //variables to slow down animation
 let gameFrame = 0;
-const staggerFrames = 6``;
+const staggerFrames = 7;
+ 
+
+const spriteAnimations = [];
+const animationStates = [
+    {
+        name: 'idle',
+        frames: 7
+    },
+    {
+        name: 'jump',
+        frames: 7
+    },
+    {
+        name: 'fall',
+        frames: 7
+    },
+    {
+        name: 'run',
+        frames: 9
+    },
+    {
+        name: 'dizzy',
+        frames: 11
+    },
+    {
+        name: 'sit',
+        frames: 5
+    },
+    {
+        name: 'roll',
+        frames: 7
+    },
+    {
+        name: 'bite',
+        frames: 7
+    },
+    {
+        name: 'ko',
+        frames: 12
+    },
+    {
+        name: 'getHit',
+        frames: 4
+    }
+];
+
+animationStates.forEach((state, index) => {
+    let frames = {
+        loc: [],
+    }
+    for (let j = 0; j < state.frames; j++) {
+        let positionX = j * spriteWidth;
+        let positionY = index * spriteHeight;
+        frames.loc.push({ x: positionX, y: positionY });
+    }
+    spriteAnimations[state.name] = frames;
+});
+
+console.log(spriteAnimations);
 
 function animate() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    //  ctx.fillRect(x,50,100,100);
-    // dimensions of drawImg can take up to 9 arguements(img(this is the source),
-    // sx,sy,swidth,sheight(these four are the crop demensions),
+    let position = Math.floor(gameFrame / staggerFrames) % spriteAnimations[playerState].loc.length;
+    let frameX = spriteWidth * position;
+    let frameY = spriteAnimations[playerState].loc[position].y;
+    // dimensions of drawImg can take up to 9 arguments(img(this is the source), 
+    // sx,sy,sWidth,sHeight(these four are the crop dimensions),
     // dx,dy,dw,dh(these four are the area it fills in the canvas));
-    ctx.drawImage(playerImage, frameX * spriteWidth, frameY * spriteHeight,
+    ctx.drawImage(playerImage, frameX, frameY,
         spriteWidth, spriteHeight, 0, 0, spriteWidth, spriteHeight);
-    if(gameFrame % staggerFrames == 0) {
-        if(frameX < 6 ) frameX++;
-        else frameX = 0;
-    }
+
     gameFrame++;
 
-    requestAnimationFrame(animate);
+    requestAnimationFrame(animate); 
 };
 
 animate();
